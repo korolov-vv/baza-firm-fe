5370164834d86bd20473176adfad2631c8da55b5
Vadym Korolov <vadymkorolov@gmail.com>
2026-02-24 20:35:49 +0100

    Update API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 production: true,
-apiUrl: 'https://baza-firm-be.internal:8082/api',
+apiUrl: 'http://baza-firm-be.internal:8082/api',
 keycloak: {
 url: 'https://keycloak-bazafirm.fly.dev',
 realm: 'bazafirm-customer',
